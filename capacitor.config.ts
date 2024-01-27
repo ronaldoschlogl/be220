5faf7a953407ce9cf742647e8707724ea00c892d
ronaldo.schlogl <ronaldo.schlogl@insidesistemas.com.br>
2024-01-27 13:33:18 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.be220',
+  appName: 'be220',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
